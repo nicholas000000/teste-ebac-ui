58fef446db34ff7858e11a205cc6ff75b35f2324
Nicholas <Nicholasrochadacosta@gmail.com>
2025-10-20 15:07:31 -0300

    primeiro teste automizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('nicholas@teste.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, nicholas (não é nicholas? Sair)')
+
+    })
+
+})
